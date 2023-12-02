43c25bda8a4c5781afdd2f4fb808e0448f1865bc
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-12-02 19:35:07 +0100

    isses almost ready

--- nktapp/src/Issues.js
+++ nktapp/src/Issues.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 import { useTheme } from '../theme/ThemeProvider';
 
 export default function Issues({navigation}) {
 
 
   const {colors} = useTheme();
+
+
+  const Issue = () => {
+    return(
+      <View style={{
+        width: '90%',
+        height: 150,
+        backgroundColor: colors.grey_l,
+        borderRadius: 10,
+        marginHorizontal: '5%',
+        marginBottom: 10
+      }}>
+  
+      </View>
+    )
+    
+  }
+
 
   return (
     <View style={styles.container}>
@@ -18,7 +36,7 @@ export default function Issues({navigation}) {
           borderRadius: 10,
           alignItems: 'center',
           justifyContent: 'center',
-          marginTop: 10
+          marginTop: 20
         }}
         onPress={() => navigation.navigate('IssueRequest')}
       >
@@ -27,6 +45,16 @@ export default function Issues({navigation}) {
           fontWeight: 'bold'
         }}>Zgłoś zdarzenie</Text>
       </TouchableOpacity>
+
+      <View style={{
+        width: '100%',
+        padding: '5%'
+      }}>
+        <Text style={{color: colors.grey_d}}>Twoje ostatnie zgłoszenia</Text>
+      </View>
+      <Issue />
+      <Issue />
+      <Issue />
     </View>
   );
 }
