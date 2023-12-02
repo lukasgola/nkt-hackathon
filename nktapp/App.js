4ee33fcbca53827ef6f966ed3af53fa5c5c30e96
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-12-02 14:34:07 +0100

    working

--- nktapp/App.js
+++ nktapp/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+
 import BottomTabs from './navigation/BottomTabs';
 
 export default function App() {
---
+++ nktapp/theme/Theme.js
@@ -0,0 +1,29 @@
+// Light theme colors
+export const lightColors = {
+    primary: '#00d295',
+    primary_l: '#d7f6ed',
+    secondary: '#f9b411',
+    secondary_l :'#fbedcc',
+    grey_l: '#f2f2f2',
+    grey: '#cbcbd7',
+    grey_d: '#74747f',
+    background: '#FFF',
+    text: '#000000',
+    container: '#F9F9F9',
+    star: '#fed600'
+  };
+  
+  // Dark theme colors
+  export const darkColors = {
+    primary: '#00d295',
+    primary_l: '#d7f6ed',
+    secondary: '#f9b411',
+    secondary_l :'#fbedcc',
+    grey_l: '#222',
+    grey: '#cbcbd7',
+    grey_d: '#74747f',
+    background: '#121212',
+    text: '#FFFFFF',
+    star: '#fed600'
+  };
+  
