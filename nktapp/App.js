2684d89a46187564bf28fecff1c85ab7b5bdfad2
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-12-02 14:37:54 +0100

    theme

--- nktapp/App.js
+++ nktapp/App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-
+import { ThemeProvider } from './theme/ThemeProvider';
 import BottomTabs from './navigation/BottomTabs';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTabs/>
+      <ThemeProvider>
+        <BottomTabs/>
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
--- nktapp/src/Home.js
+++ nktapp/src/Home.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useTheme } from '../theme/ThemeProvider';
+
 export default function App() {
+
+    const {colors} = useTheme();
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
