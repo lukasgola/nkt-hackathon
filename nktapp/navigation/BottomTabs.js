fe6025f47c660aec8de7401e8b3b0ba4adcd110a
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-12-02 14:26:46 +0100

    bottom stack

--- nktapp/navigation/BottomTabs.js
+++ nktapp/navigation/BottomTabs.js
@@ -3,11 +3,15 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab = createBottomTabNavigator();
 
 import Home from '../src/Home';
+import History from '../src/History';
+import Issues from '../src/Issues';
 
 export default function BottomTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="Issues" component={Issues} />
     </Tab.Navigator>
   );
 }
